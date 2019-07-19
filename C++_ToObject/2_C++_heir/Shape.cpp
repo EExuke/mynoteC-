6604da0563989542f42d3c71f241f52b9cf06f73
xuke <iceke@foxmail.com>
2019-07-19 18:43:03 +0800

    heir and reload

---
+++ C++_ToObject/2_C++_heir/Shape.cpp
@@ -0,0 +1,56 @@
+/**************************************************************************** **
+ * 一个类可以派生自多个类，这意味着，它可以从多个基类继承数据和函数。
+ * 定义一个派生类，我们使用一个类派生列表来指定基类。
+ * 类派生列表以一个或多个基类命名，形式如下：
+ *  	class derived-class: access-specifier base-class
+ * 其中，访问修饰符 access-specifier 是 public、protected 或 private 其中的
+ * 一个，base-class 是之前定义过的某个类的名称。
+ * 如果未使用访问修饰符 access-specifier，则默认为 private。
+ *
+ * 假设有一个基类 Shape，Rectangle 是它的派生类，如下所示：
+ **************************************************************************** **/
+
+#include <iostream>
+
+using namespace std;
+
+// 基类
+class Shape 
+{
+	public:
+		void setWidth(int w)
+		{
+			width = w;
+		}
+		void setHeight(int h)
+		{
+			height = h;
+		}
+	protected:
+		int width;
+		int height;
+};
+
+// 派生类
+class Rectangle: public Shape
+{
+	public:
+		int getArea()
+		{ 
+			return (width * height); 
+		}
+};
+
+int main(void)
+{
+	Rectangle Rect;
+
+	Rect.setWidth(5);
+	Rect.setHeight(7);
+
+	// 输出对象的面积
+	cout << "Total area: " << Rect.getArea() << endl;
+
+	return 0;
+}
+
